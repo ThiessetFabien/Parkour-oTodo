8a409474f4eb7ab7be2e9a6ee20ce5a960fe1287
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-03-13 09:21:05 +0100

    feet: route getAllTasks

--- back/app/controllers/task.js
+++ back/app/controllers/task.js
@@ -2,8 +2,17 @@ import { Task } from "../models/Task.js";
 
 export async function getAllTasks(req, res) {
   // Récupérer la liste des tâches
-  const tasks = []; // TODO !
-
+  try {
+    const tasks = await Task.findAll({
+      order: [["created_at", "DESC"]],
+    });
+    console.log('tasks ', tasks);
+    res.json(tasks);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Unexpected server error. Please try again later." });
+  }
   // Renvoyer la liste des tâches au format JSON
-  res.json(tasks);
 }
