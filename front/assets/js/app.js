d961d2a8a1b988bf80af51a4bf33951068fd4624
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-03-13 09:44:57 +0100

    feet: fetch all tasks

--- front/assets/js/app.js
+++ front/assets/js/app.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", init);
 
 async function init() {
   // Ajout de donner de tests. [TODO] A SUPPRIMER lorsqu'on aura fait les appels API
-  taskManager.insertTaskInHTML({ id: 1, name: "Créer le HTML" });
-  taskManager.insertTaskInHTML({ id: 2, name: "Créer le CSS" });
+  // taskManager.insertTaskInHTML({ id: 1, name: "Créer le HTML" });
+  // taskManager.insertTaskInHTML({ id: 2, name: "Créer le CSS" });
 
   // Fetch et afficher les tâches
   await taskManager.fetchAndInsertTasks();
--- front/assets/js/task.js
+++ front/assets/js/task.js
@@ -2,10 +2,19 @@ import { apiBaseUrl } from "./config.js";
 
 export async function fetchAndInsertTasks() {
   // Récupérer les listes via l'API à l'aide de la fonction `fetch()`
+  const response = await fetch(`${apiBaseUrl}/tasks`);
+  const tasks = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Une erreur est survenue");
+  }
 
   // Boucler sur la liste des tâches
-  
-  // Pour chaque tâche, l'insérer dans la page à l'aide de la fonction `insertTaskInHTML()`
+  tasks.forEach((task) => {
+    // Pour chaque tâche, l'insérer dans la page à l'aide de la fonction `insertTaskInHTML()`
+    console.log('task ', task);
+    insertTaskInHTML(task);
+  });
 }
 
 /**
@@ -14,35 +23,34 @@ export async function fetchAndInsertTasks() {
  * @param {number} taskData.id - L'id de la tâche
  * @param {string} taskData.name - Le nom de la tâche
  * @example insertTaskInHTML({ id: 1, name: "Faire les courses"}); // Affiche une tâche dans la page
-*/
+ */
 export function insertTaskInHTML(taskData) {
   // On récupère le HTML d'une tâche dans le template
-  const taskTemplate = document.querySelector('.template-task');
+  const taskTemplate = document.querySelector(".template-task");
   const newTask = document.importNode(taskTemplate.content, true);
 
   // On insère les données de la tâche dans le HTML
-  newTask.querySelector('.task__name').textContent = taskData.name;
-  newTask.querySelector('.task__input-name').value = taskData.name;
-  newTask.querySelector('.task__input-id').value = taskData.id;
-  newTask.querySelector('.task').dataset.id = taskData.id;
+  newTask.querySelector(".task__name").textContent = taskData.name;
+  newTask.querySelector(".task__input-name").value = taskData.name;
+  newTask.querySelector(".task__input-id").value = taskData.id;
+  newTask.querySelector(".task").dataset.id = taskData.id;
 
   // On écoute les événements sur les éléments créés
   newTask
-    .querySelector('.task__delete')
-    .addEventListener('click', handleDeleteButton);
-    
-  newTask
-    .querySelector('.task__edit')
-    .addEventListener('click', handleEditButton);
+    .querySelector(".task__delete")
+    .addEventListener("click", handleDeleteButton);
 
   newTask
-    .querySelector('.task__edit-form')
-    .addEventListener('submit', handleEditForm);
+    .querySelector(".task__edit")
+    .addEventListener("click", handleEditButton);
+
+  newTask
+    .querySelector(".task__edit-form")
+    .addEventListener("submit", handleEditForm);
 
   // On insère le HTML de la tâche dans la page
-  document.querySelector('.tasks').append(newTask);
+  document.querySelector(".tasks").append(newTask);
 }
-
 
 export function handleCreateForm(event) {
   // Bloquer l'envoie du formulaire
@@ -54,13 +62,13 @@ export function handleCreateForm(event) {
 
   // Envoyer les données à l'API
 
-  // Après confirmation de l'API insérer la tâche dans la page (il y a une fonction toute prete pour ça ;) 
+  // Après confirmation de l'API insérer la tâche dans la page (il y a une fonction toute prete pour ça ;)
   // en utilisant la valeur de retour de l'API
 }
 
 function handleDeleteButton(event) {
   // On récupère l'ID de l'élément à supprimer
-  const taskHtmlElement = event.currentTarget.closest('.task');
+  const taskHtmlElement = event.currentTarget.closest(".task");
   const taskId = parseInt(taskHtmlElement.dataset.id);
   console.log(taskId);
 
@@ -71,11 +79,11 @@ function handleDeleteButton(event) {
 
 function handleEditButton(event) {
   // On récupére l'élément HTML de la tâche à modifier
-  const taskHtmlElement = event.currentTarget.closest('.task');
+  const taskHtmlElement = event.currentTarget.closest(".task");
   // On affiche l'input de modification
-  taskHtmlElement.querySelector('.task__edit-form').style.display = 'flex';
+  taskHtmlElement.querySelector(".task__edit-form").style.display = "flex";
   // On masque le titre
-  taskHtmlElement.querySelector('.task__name').style.display = 'none';
+  taskHtmlElement.querySelector(".task__name").style.display = "none";
 }
 
 function handleEditForm(event) {
@@ -83,11 +91,11 @@ function handleEditForm(event) {
   event.preventDefault();
 
   // On récupère l'élément HTML complet de la tâche à modifier
-  const taskHtmlElement = event.currentTarget.closest('.task');
+  const taskHtmlElement = event.currentTarget.closest(".task");
 
   // Récupérer les données du formulaire
   const editTaskFormData = new FormData(event.currentTarget);
-  
+
   console.log(editTaskFormData.get("name")); // Le nouveau nom récupéré
   console.log(editTaskFormData.get("id")); // L'ID de la tâche à modifier
 
@@ -96,7 +104,7 @@ function handleEditForm(event) {
   // Après confirmation de l'API modifier le nom de la tâche dans le span.task__name
 
   // On affiche l'input de modification
-  taskHtmlElement.querySelector('.task__edit-form').style.display = 'none';
+  taskHtmlElement.querySelector(".task__edit-form").style.display = "none";
   // On masque le titre
-  taskHtmlElement.querySelector('.task__name').style.display = 'block';
+  taskHtmlElement.querySelector(".task__name").style.display = "block";
 }
